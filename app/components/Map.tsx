dfbf33d5ea88d556ce52729d002fac5f3ec12ffe
awulz <andrin.wulz@gmail.com>
2026-02-04 19:55:40 +0100

    #5 Add click-to-place-marker functionality

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,13 +1,42 @@
 'use client'
 
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { useState } from 'react'
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import { LatLngExpression } from 'leaflet'
+import { LatLngExpression, icon } from 'leaflet'
 
 const SWITZERLAND_CENTER: LatLngExpression = [46.8182, 8.2275]
 const DEFAULT_ZOOM = 8
 
+// Fix for default marker icon in production
+const defaultIcon = icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+})
+
+function MapClickHandler({ onLocationSelect }: { onLocationSelect: (lat: number, lng: number) => void }) {
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng
+      console.log('Clicked coordinates:', { lat, lng })
+      onLocationSelect(lat, lng)
+    },
+  })
+  return null
+}
+
 export default function Map() {
+  const [markerPosition, setMarkerPosition] = useState<LatLngExpression | null>(null)
+
+  const handleLocationSelect = (lat: number, lng: number) => {
+    setMarkerPosition([lat, lng])
+  }
+
   return (
     <MapContainer
       center={SWITZERLAND_CENTER}
@@ -19,6 +48,8 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapClickHandler onLocationSelect={handleLocationSelect} />
+      {markerPosition && <Marker position={markerPosition} icon={defaultIcon} />}
     </MapContainer>
   )
 }
